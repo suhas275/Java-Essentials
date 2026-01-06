20ab5a78f19719c11047070994424b0fb0119388
suhas <suhasgowdam275@gmail.com>
2026-01-06 22:57:20 +0530

    initializing file

---
+++ Java via code/src/variableNomenclature.java
@@ -0,0 +1,25 @@
+public class variableNomenclature {
+
+    public static void main(String[] args){
+        int decimalLiteral = 8;
+
+        int octalLiteral = 012; //The octal number system is a base-8 system using digits (0–7): 0, 1, 2, 3, 4, 5, 6, and 7.
+
+        int hexadecimal = 0XAC; //Hexadecimal numbers use 16 symbols or digital values: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F
+
+        long longLiterals = 24000000000L;  //[need to suffix the number with 'L' ]
+
+        float price = 26.3f;
+
+        float weight = 25e-2f; //scientific notation
+
+        double distance = 123.9;
+
+        char a = 'A';
+
+        boolean condition = true;
+
+
+        System.out.println(a);
+    }
+}
